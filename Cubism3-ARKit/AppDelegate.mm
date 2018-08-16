9e0e4ffb371768306edada2a00742b0725ea9c4b
Yuma Matsune <yuma.matsune@gmail.com>
2018-08-17 02:46:19 +0900

    alloc and release cubism

---
+++ Cubism3-ARKit/AppDelegate.mm
@@ -0,0 +1,73 @@
+//
+//  AppDelegate.m
+//  Cubism3-ARKit
+//
+//  Created by Yuma Matsune on 2018/08/17.
+//  Copyright © 2018 matsune. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import "LAppAllocator.h"
+
+#import "CubismFramework.hpp"
+#import "LAppPal.h"
+
+@interface AppDelegate ()
+
+@property (nonatomic) LAppAllocator cubismAllocator; // Cubism3 Allocator
+@property (nonatomic) Csm::CubismFramework::Option cubismOption; // Cubism3 Option
+
+@end
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    _window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    _viewController = [[ViewController alloc] initWithNibName:nil bundle:nil];
+    _window.rootViewController = _viewController;
+    [_window makeKeyAndVisible];
+    [_window setBackgroundColor:[UIColor whiteColor]];
+    return YES;
+}
+
+- (void) initializeCubism {
+    _cubismOption.LogFunction = LAppPal::PrintMessage;
+    _cubismOption.LoggingLevel = Live2D::Cubism::Framework::CubismFramework::Option::LogLevel_Verbose;
+    
+    Csm::CubismFramework::StartUp(&_cubismAllocator,&_cubismOption);
+    Csm::CubismFramework::Initialize();
+    
+//    [LAppLive2DManager getInstance];
+    
+    LAppPal::UpdateTime();
+}
+
+- (void) releaseCubism {
+    Csm::CubismFramework::Dispose();
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+
+}
+
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    
+}
+
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    
+}
+
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+}
+
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    [self releaseCubism];
+}
+
+
+@end
